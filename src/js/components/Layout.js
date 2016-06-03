5794f6ec777c1709a63a65578b2868ad89038bb1
Andrew Thomas <andr3w.l.thomas@gmail.com>
2016-06-03 18:34:00 -0500

    removed footer component, not necessary

--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -1,16 +1,16 @@
 import React from "react";
 
-import Footer from './Footer';
 import Header from './Header';
 
 export default class Layout extends React.Component {
-  render() {
-    return (
-      <div>
-        <Header />
-        <main class="container"> {this.props.children} </main>
-        <Footer />
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <Header />
+                <main class="container">
+                    {this.props.children}
+                </main>
+            </div>
+        );
+    }
 }
