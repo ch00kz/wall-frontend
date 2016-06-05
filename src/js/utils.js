24ed42e498d9ae3ff64fe3701dbb86967e334a6e
Andrew Thomas <andr3w.l.thomas@gmail.com>
2016-06-05 14:24:14 -0500

    created an httpClient to set baseUrl and automatically add the auth header (after checking auth store for user) if necessary

---
+++ src/js/utils.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+import  AuthStore from "./stores/AuthStore";
+
+// have to do this as a function so we always have oen with proper headers,
+// it will always check the authstore for the current user
+
+function httpClient() {
+    const headers = {}
+    const token = AuthStore.getToken();
+    console.log("building new client with token", token);
+    if (token){
+        headers['Authorization'] = 'Token ' + token;
+    }
+
+    const client = axios.create({
+        baseURL: 'http://192.168.1.136:9000',
+        headers
+    });
+    return client
+}
+
+export default httpClient
