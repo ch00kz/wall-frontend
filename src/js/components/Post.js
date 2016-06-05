309760985ddfa1aa9cbca7f5d5fcc8d7d696adee
Andrew Thomas <andr3w.l.thomas@gmail.com>
2016-06-04 22:25:00 -0500

    Added moment to humanize time being shown

--- src/js/components/Post.js
+++ src/js/components/Post.js
@@ -1,19 +1,22 @@
 import React from "react";
+import moment from "moment";
 
 export default class Post extends React.Component {
     render() {
+        const date = moment(this.props.date).fromNow()
         return (
             <article class="post">
                 <div class="post-header">
-                    <div class="likes">
+                    <div class="like">
                         <button>
                             <i class="fa fa-heart-o"></i>
                         </button>
                     </div>
                     <div class="user">{this.props.user}</div>
-                    <div class="emotion">
-                        {this.props.emotion}
-                        {this.props.likes}
+                    <div class="stats">
+                        <span class="likes">{this.props.likes + " Likes"}</span>
+                        <i class="fa fa-minus" aria-hidden="true"></i>
+                        <span class="date">{date}</span>
                     </div>
                 </div>
                 <div class="post-body">{this.props.content}</div>
